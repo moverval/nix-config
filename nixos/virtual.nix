c0f76d4d095c44fe713be9963b2f61b899ca203c
Moritz <moritz@voulter.com>
2026-07-17 14:12:10 +0200

    Add docker support

--- nixos/virtual.nix
+++ nixos/virtual.nix
@@ -9,5 +9,11 @@
     "qemu-libvirtd"
   ];
 
-  virtualisation.podman.enable = true;
+  virtualisation.containers.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
+    dockerSocket.enable = true;
+  };
 }
